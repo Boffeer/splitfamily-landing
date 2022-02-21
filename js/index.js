30eac183d43e4e41cc6f342bd26fbe8283af6812
Boffeer <41575886+Boffeer@users.noreply.github.com>
2022-02-21 21:05:33 +0300

    add schema org and show-more buttons

--- js/index.js
+++ js/index.js
@@ -220,3 +220,94 @@ const psychoCallback = document.querySelector(".psycho__button");
 psychoCallback.addEventListener("click", (event) => {
   handlePoppaButtonClick(event.target, document.querySelector(".psycho__form"));
 });
+
+/**
+ * @timer
+ */
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    total: t,
+    days: days,
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds,
+  };
+}
+
+function initializeClock(timerClass, endtime) {
+  var clock = document.querySelector(timerClass);
+  // var daysSpan = clock.querySelector('.timer-counter__left');
+  var hoursSpan = clock.querySelector(".timer-counter__left");
+  var minutesSpan = clock.querySelector(".timer-counter__center");
+  var secondsSpan = clock.querySelector(".timer-counter__right");
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    // daysSpan.innerHTML = t.days;
+
+    hoursSpan.innerHTML = ("0" + t.hours).slice(-2);
+    minutesSpan.innerHTML = ("0" + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ("0" + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      // clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+const HOURS = 24;
+const MINUTES = 60;
+const SECONDS = 60;
+const MILLISECONDS = 1000;
+if (localStorage.getItem("localTimer") == null) {
+  localStorage.setItem(
+    "localTimer",
+    new Date(Date.parse(new Date()) + HOURS * MINUTES * SECONDS * MILLISECONDS)
+  );
+}
+console.log(localStorage.getItem("localTimer"));
+
+var deadline = localStorage.getItem("localTimer");
+// var deadline = new Date(Date.parse(new Date()) + 10 * 1000); // for endless timer
+initializeClock(".timer-counter", deadline);
+
+/**
+ * @showMore
+ */
+
+function showMore(clickTrigger, classesArray, initialShow) {
+  const classHidden = "show-more__hidden";
+  const elements = document.querySelectorAll(classesArray);
+  elements.forEach((item, index) => {
+    if (index > initialShow - 1) {
+      item.classList.add(classHidden);
+    }
+  });
+
+  clickTrigger.addEventListener("click", (event) => {
+    event.target.style.display = "none";
+    elements.forEach((item) => {
+      if (item.classList.value.includes(classHidden)) {
+        item.classList.remove(classHidden);
+      }
+    });
+  });
+}
+
+const casesShowMore = document.querySelector(".cases__button");
+showMore(casesShowMore, ".cases-accordion", 2);
+
+const pricingShowMore = document.querySelector(".pricing__show-more");
+if (window.innerWidth < 575) {
+  showMore(pricingShowMore, ".pricing-slider-slide", 2);
+}
